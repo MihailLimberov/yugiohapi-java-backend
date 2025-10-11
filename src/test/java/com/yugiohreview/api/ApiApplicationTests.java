ba47d7b2305cb788279771d4eaecead215a837eb
ELEMAG <m.d.limberov@gmail.com>
2025-10-11 21:32:55 +0300

    initialize project

---
+++ src/test/java/com/yugiohreview/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yugiohreview.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
